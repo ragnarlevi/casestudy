4382872f96155601183c602aab9d1d2638c352e0
ragnarlevi <ragnarlevigud@gmail.com>
2019-01-27 13:05:28 +0000

    More graphs

--- caseStudy2019.R
+++ caseStudy2019.R
@@ -20,9 +20,15 @@ names(autoclaims)
 tmp <- c("Year", "Qtr", "RiskClass", "Type", "CarYearsOfExposure", "NoBodilyInjury", "NoPropertyDamage",
          "NoComprehensive", "NoCollision", "NoPersonalInjury", "AmountBodilyInjury", "AmountPropertyDamage",
          "AmountComprehensive", "AmountCollision", "AmountPersonalInjury")
+
+variables <- c("CarYearsOfExposure", "NoBodilyInjury", "NoPropertyDamage",
+"NoComprehensive", "NoCollision", "NoPersonalInjury", "AmountBodilyInjury", "AmountPropertyDamage",
+"AmountComprehensive", "AmountCollision", "AmountPersonalInjury")
 names(autoclaims) <- tmp
 head(autoclaims)
 
+# add year + qtr
+autoclaims$YearQtr <- autoclaims$Year + 2.5*autoclaims$Qtr/10 
 
 # Total Exposure for each qtr
 autoclaims$TotalExposure <- 0
@@ -37,16 +43,43 @@ for( i in levels(factor(autoclaims$Year))){
 # then we normalize
 autoclaims$ExposureNormalize <- autoclaims$CarYearsOfExposure/autoclaims$TotalExposure
 
+View(autoclaims[autoclaims$RiskClass == "SML" & autoclaims$Type == "Personal",])
+
+
 # lets make three plots for each vehicle size 
 
-ggplot(data = au)
+ggplot(data = autoclaims[grepl("^S", x = autoclaims$RiskClass) & autoclaims$Type  == "Personal", , drop = F]) + geom_line(aes( x = YearQtr, y = ExposureNormalize , color = RiskClass))
+
+ggplot(data = autoclaims[grepl("^M", x = autoclaims$RiskClass) & autoclaims$Type  == "Personal", , drop = F]) + geom_line(aes( x = YearQtr, y = ExposureNormalize , color = RiskClass))
+
+ggplot(data = autoclaims[grepl("^L", x = autoclaims$RiskClass) & autoclaims$Type  == "Personal", , drop = F]) + geom_line(aes( x = YearQtr, y = ExposureNormalize , color = RiskClass))
+
+# Amount comprehensive
+
+# Super spike
+melt.autoclaims <- melt(autoclaims, id = c("Year", "Qtr", "YearQtr", "Type", "RiskClass"))
+ggplot(data = melt.autoclaims[ melt.autoclaims$Type  == "Personal" & !(melt.autoclaims$variable %in% c("TotalExposure", "ExposureNormalize"))
+                               ,  , drop = F]) + geom_line(aes( x = YearQtr, y = value , color = RiskClass))+ facet_wrap( ~ variable, scales = "free")
+
+
+# But if we aggregate The Risk classes
+autoclaims.Qtrly <- aggregate(. ~ Year + Qtr+ YearQtr, autoclaims[, !(names(autoclaims) %in% c("Type", "RiskClass") )], FUN = sum)
+
+melt.autoclaims.Qtrly <- melt(autoclaims.Qtrly, id = c("Year", "Qtr", "YearQtr"))
+ggplot(data = melt.autoclaims.Qtrly[ !(melt.autoclaims.Qtrly$variable %in% c("TotalExposure", "ExposureNormalize", "NoPropertyDamage",
+                                                                             "NoComprehensive", "NoCollision", "NoBodilyInjury", "NoPersonalInjury", "CarYearsOfExposure")), ]) + geom_line(aes( x = YearQtr, y = value, color = variable )) 
+
+ggplot(data = melt.autoclaims.Qtrly[ !(melt.autoclaims.Qtrly$variable %in% c("TotalExposure", "ExposureNormalize", "AmountPropertyDamage",
+                                                                             "AmountComprehensive", "AmountCollision", "AmountBodilyInjury", "AmountPersonalInjury")), ]) + geom_line(aes( x = YearQtr, y = value, color = variable )) 
+
+
+ggplot(data = melt.autoclaims.Qtrly[ !(melt.autoclaims.Qtrly$variable %in% c("TotalExposure", "ExposureNormalize", "AmountPropertyDamage",
+                                                                             "AmountComprehensive", "AmountCollision", "AmountBodilyInjury", "AmountPersonalInjury", "CarYearsOfExposure")), ]) + geom_line(aes( x = YearQtr, y = value, color = variable )) 
 
 
 
 
-# add year + qtr
-autoclaims$YearQtr <- autoclaims$Year + 2.5*autoclaims$Qtr/10
-
+#  ----
 names(autoclaims)
 # function for lower part of ggpairs data
 lowerFn <- function(data, mapping, method , ...) {
@@ -56,7 +89,6 @@ lowerFn <- function(data, mapping, method , ...) {
   p
 }
 
-attach(autoclaims)
 
 
 ggpairs(autoclaims[, c(4,6:dimension[2])],lower = list(continuous = wrap(lowerFn, method = "loess")), 
@@ -106,9 +138,24 @@ ggplot(data = melt.auto.agg.type) + geom_line(aes( x = YearQtr, y = value, color
 # Remember agg.type is the aggregated personal and commercial
 View(autoclaims.agg.type)
 
-ggplot(data = autoclaims.agg.type) + geom_line(aes( x = YearQtr, y = NoBodilyInjury , color = RiskClass))
-ggplot(data = autoclaims.agg.type) + geom_line(aes( x = YearQtr, y = CarYearsOfExposure , color = RiskClass))
+ggplot(data = autoclaims.agg.type) + geom_line(aes(x = YearQtr, y = NoBodilyInjury , color = RiskClass))
+ggplot(data = autoclaims.agg.type) + geom_line(aes(x = YearQtr, y = CarYearsOfExposure , color = RiskClass))
   
+
+
+# histogram
+ggplot(data = autoclaims) + geom_histogram(aes(NoBodilyInjury))
+
+sum(NoBodilyInjury == 1)
+
+### ---- Some nice Proportion plots
+
+
+#-----
+ggplot(data = autoclaims[autoclaims$Type == "Personal", , drop = F]) + geom_histogram(aes(NoBodilyInjury), bins = 30)
+
+
+ggplot(data = autoclaims[autoclaims$Type == "Personal" & autoclaims$RiskClass == "SML", , drop = F]) + geom_histogram(aes(NoBodilyInjury), bins = 30)
 
 
 

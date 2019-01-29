c5b89de187cd898b0d1b6260438f612e024f8c9b
ragnarlevi <ragnarlevigud@gmail.com>
2019-01-29 17:14:51 +0000

    Outlier vs refined data

--- GLM.R
+++ GLM.R
@@ -70,21 +70,97 @@ autocar$time <- autocar$Year + 2.5*as.numeric(autocar$Qtr)/10
 
 autocar$time
 
-melt.autocar <- melt(autocar[, names(autocar) %in% c("time", "AC_COM", "RiskClass", "Type")] , id = c("Type", "RiskClass", "time"))
-ggplot(melt.autocar[melt.autocar$Type == "Commercial",]) + geom_line(aes(x = time, y = value, color = RiskClass)) +
-  geom_point(aes(2010.75,0))
+melt.autocar <- melt(autocar[, names(autocar) %in% c("time", "AC_COM", "RiskClass", "Type", "NC_COM")] , id = c("Type", "RiskClass", "time"))
+ggplot(melt.autocar[melt.autocar$Type == "Commercial",]) + geom_line(aes(x = time, y = value, color = RiskClass))  + facet_wrap( ~ variable)
 
 # linear regression for amount comprehensive and number comprehnsive
-lm <- list()
+lm.no.outlier <- list()
 autocar.tmp <- autocar[autocar$time != 2010.75,]
-View(autocar.tmp)
+#View(autocar.tmp)
 
 
-# need to do so for each risk class, Put it in a list and make it with a forloop, and for 
+# need to do so for each risk class and each type, Put it in a list and make it with a for loop
 
-for(i in autocar.tmp$RiskClass){
-  lm[[i]] <- lm(AC_COM ~ factor(time)+ factor(Qtr), data = autocar.tmp[])
+for(j in unique(as.character(autocar.tmp$Type))){
+  # we need to use unique because we only want to do the linear regression one
+  for(i in unique(as.character(autocar.tmp$RiskClass))){
+    lm.no.outlier$AC_COM[[j]][[i]] <- lm(AC_COM ~ Year+ factor(Qtr), data = autocar.tmp[autocar.tmp$Type == j & autocar.tmp$RiskClass == i, ])
+    lm.no.outlier$NC_COM[[j]][[i]] <- lm(NC_COM ~ Year+ factor(Qtr), data = autocar.tmp[autocar.tmp$Type == j & autocar.tmp$RiskClass == i, ])
+  }
   
 }
 
+# Then We predict and put into new data frame called ..
+autocar.refined <- autocar
+# defining our predict var
+predict.val <- data.frame(Year = 2010, Qtr = 3)
+
+for(j in unique(as.character(autocar.tmp$Type))){
+  # we need to use unique because we only want to do the linear regression one
+  for(i in unique(as.character(autocar.tmp$RiskClass))){
+    # PREDICT FOR AC_COM
+    autocar.refined$AC_COM[autocar.refined$Type == j & autocar$RiskClass == i & autocar.refined$Year == 2010 & autocar.refined$Qtr ==3] <- 
+      predict.lm(lm.no.outlier$AC_COM[[j]][[i]], predict.val)
+    # Predict for NC_com and we round up to get integer
+    autocar.refined$NC_COM[autocar.refined$Type == j & autocar$RiskClass == i & autocar.refined$Year == 2010 & autocar.refined$Qtr ==3] <-
+      ceiling(predict.lm(lm.no.outlier$NC_COM[[j]][[i]], predict.val))
+  }
+  
+}
+
+# Compare
+melt.autocar.refined <- melt(autocar.refined[, names(autocar.refined) %in% c("time", "AC_COM", "RiskClass", "Type", "NC_COM")] , id = c("Type", "RiskClass", "time"))
+
+# Figure For Personal types
+p1 <- ggplot(melt.autocar[melt.autocar$Type == "Personal",]) + geom_line(aes(x = time, y = value, color = RiskClass)) +
+  geom_vline(xintercept = 2010.75) + facet_wrap( ~ variable, scales = "free") + ggtitle("Personal raw data")
+p2 <- ggplot(melt.autocar.refined[melt.autocar.refined$Type == "Personal",]) + geom_line(aes(x = time, y = value, color = RiskClass)) +
+  geom_vline(xintercept = 2010.75) + facet_wrap( ~ variable, scales = "free") + ggtitle("Personal refined data")
+
+# Figure for Commercial types
+p3 <- ggplot(melt.autocar[melt.autocar$Type == "Commercial",]) + geom_line(aes(x = time, y = value, color = RiskClass)) +
+  geom_vline(xintercept = 2010.75) + facet_wrap( ~ variable, scales = "free") + ggtitle("Commercial raw data")
+p4 <- ggplot(melt.autocar.refined[melt.autocar.refined$Type == "Commercial",]) + geom_line(aes(x = time, y = value, color = RiskClass)) +
+  geom_vline(xintercept = 2010.75) + facet_wrap( ~ variable, scales = "free") + ggtitle("Commercial refined data")
+
+# borrowed this function
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+multiplot(p1,p2, plotlist = NULL, cols = 1)
+
+multiplot(p3,p4, plotlist = NULL, cols = 1)
 

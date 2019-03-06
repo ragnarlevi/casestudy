1aae95b48b7c8dab4fe182ae6d8a06927cc61bfa
ragnarlevi <ragnarlevigud@gmail.com>
2019-03-06 16:59:38 +0000

    Plot fun

--- Scenarios.R
+++ Scenarios.R
@@ -14,65 +14,86 @@ source("functions.R")
 # Call the GLM.R script, estimates number and AAC for current automoblies. Also
 source("GLM.R")
 
+time.frame <- 0:11
+start.year <- 2019
+t <- seq(min(time.frame), max(time.frame), by = 0.25)
+source("Base.R")
 
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_Autonomous_Vehicle_Percentage_Best.R")
-Auto_Exposure_pct_Best$Scenario <- "Autonomous % Best shock"
+Auto_Exposure_pct_Best$Scenario <- "Autonomous % of Carbia"
+Auto_Exposure_pct_Best$Case <- "Best"
+Base_Exp_pct <- Base
+Base_Exp_pct$Scenario <-  "Autonomous % of Carbia"
+Base_Exp_pct$Case <- "Base"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_Autonomous_Vehicle_Percentage_Worst.R")
-Auto_Exposure_pct_Worst$Scenario <- "Autonomous % Worst shock"
+Auto_Exposure_pct_Worst$Scenario <- "Autonomous % of Carbia"
+Auto_Exposure_pct_Worst$Case <- "Worst"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_multiplier_best.R")
-Auto_Multiplier_Best$Scenario <- "Multipliers Best"
+Auto_Multiplier_Best$Scenario <- "Multipliers"
+Auto_Multiplier_Best$Case <- "Best"
+Base_mult <- Base
+Base_mult$Scenario <-  "Multipliers"
+Base_mult$Case <- "Base"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_multiplier_worst.R")
-Auto_Multiplier_Worst$Scenario <- "Multipliers Worst"
+Auto_Multiplier_Worst$Scenario <- "Multipliers"
+Auto_Multiplier_Worst$Case <- "Worst"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_new_coverge_Best.R")
-Auto_Coverage_Best$Scenario <- "Coverage Best"
+Auto_Coverage_Best$Scenario <- "Coverage"
+Auto_Coverage_Best$Case <- "Best"
+Base_Coverage <- Base
+Base_Coverage$Scenario <-  "Coverage"
+Base_Coverage$Case <- "Base"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_new_coverge_Worst.R")
-Auto_Coverage_Worst$Scenario <- "Coverage Worst"
+Auto_Coverage_Worst$Scenario <- "Coverage"
+Auto_Coverage_Worst$Case <- "Worst"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source(file = "Auto_safelife_marketshare_Best.R")
-Auto_safelife_ms_Best$Scenario <- "Safelife Marketshare Best"
+Auto_safelife_ms_Best$Scenario <- "Safelife Marketshare"
+Auto_safelife_ms_Best$Case <- "Best"
+Base_safelife_ms <- Base
+Base_safelife_ms$Scenario <-  "Safelife Marketshare"
+Base_safelife_ms$Case <- "Base"
 
 time.frame <- 0:11
 start.year <- 2019
 t <- seq(min(time.frame), max(time.frame), by = 0.25)
 source("Auto_safelife_marketshare_Worst.R")
-Auto_Safelife_ms_Worst$Scenario <- "Safelife Marketshare Worst"
+Auto_Safelife_ms_Worst$Scenario <- "Safelife Marketshare"
+Auto_Safelife_ms_Worst$Case <- "Worst"
 
-time.frame <- 0:11
-start.year <- 2019
-t <- seq(min(time.frame), max(time.frame), by = 0.25)
-source("Base.R")
-Base$Scenario <- "Base"
 
-max(tmp.all$time[tmp.all$Scenario == "Safelife Marketshare Worst"])
 
-history <- Base[Base$time <= 2019, ]
+history <- rbind(Base_safelife_ms[Base_safelife_ms$time <= 2019, ],
+                 Base_Coverage[Base_Coverage$time <= 2019, ],
+                 Base_Exp_pct[Base_Exp_pct$time <= 2019, ],
+                 Base_mult[Base_mult$time <= 2019, ])
 
 all <- rbind(Auto_Coverage_Best[Auto_Coverage_Best$time >= 2019, ],
              Auto_Coverage_Worst[Auto_Coverage_Worst$time >= 2019, ],
@@ -82,27 +103,31 @@ all <- rbind(Auto_Coverage_Best[Auto_Coverage_Best$time >= 2019, ],
              Auto_Multiplier_Worst[Auto_Multiplier_Worst$time >= 2019, ],
              Auto_safelife_ms_Best[Auto_safelife_ms_Best$time >= 2019,],
              Auto_Safelife_ms_Worst[Auto_Safelife_ms_Worst$time >= 2019,],
-             Base[Base$time >= 2019,])
+             Base_safelife_ms[Base_safelife_ms$time >= 2019, ],
+             Base_Coverage[Base_Coverage$time >= 2019, ],
+             Base_Exp_pct[Base_Exp_pct$time >= 2019, ],
+             Base_mult[Base_mult$time >= 2019, ])
 
-tmp.history <- history[, c("time", "Scenario", "AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]
-tmp.all <- all[, c("time", "Scenario", "AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]
-
-
-tmp.history <- cbind(tmp.history[, c("time", "Scenario")], rowSums(tmp.history[, c("AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]))
-tmp.all <- cbind(tmp.all[, c("time", "Scenario")], rowSums(tmp.all[, c("AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]))
-
-names(tmp.history) <- c("time", "Scenario", "Pure Premium")
-names(tmp.all) <- c("time", "Scenario", "Pure Premium")
+tmp.history <- history[, c("time", "Case", "Scenario", "AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]
+tmp.all <- all[, c("time", "Case", "Scenario", "AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]
 
 
-tmp.all <- aggregate(. ~ time + Scenario, data = tmp.all, FUN = sum)
-tmp.history <- aggregate(. ~ time + Scenario, data = tmp.history, FUN = sum)
+tmp.history <- cbind(tmp.history[, c("time", "Case", "Scenario")], rowSums(tmp.history[, c("AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]))
+tmp.all <- cbind(tmp.all[, c("time", "Case", "Scenario")], rowSums(tmp.all[, c("AC_BI_PV", "AC_PD_PV", "AC_COM_PV", "AC_COL_PV", "AC_PI_PV", "AC_MR_PV", "AC_CR_PV", "AC_IS_PV")]))
+
+names(tmp.history) <- c("time", "Case", "Scenario", "Pure Premium")
+names(tmp.all) <- c("time", "Case", "Scenario", "Pure Premium")
+
+
+tmp.all <- aggregate(. ~ time + Scenario+ Case, data = tmp.all, FUN = sum)
+tmp.history <- aggregate(. ~ time + Scenario + Case, data = tmp.history, FUN = sum)
 
 
 ggplot() + 
-  geom_line(data = tmp.history, mapping = aes(x = time, y = `Pure Premium`, color = Scenario)) +
-  geom_line(data = tmp.all, mapping = aes(x = time, y = `Pure Premium`, color = Scenario)) +
-  theme_bw() 
+  geom_line(data = tmp.history, mapping = aes(x = time, y = `Pure Premium`, color = Case)) +
+  geom_line(data = tmp.all, mapping = aes(x = time, y = `Pure Premium`, color = Case)) +
+  theme_bw()  + 
+  facet_wrap(. ~ Scenario)
   
 
 
